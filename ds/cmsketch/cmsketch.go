9b2ac96c056b3414b702c9e8ef4c17bf85cbe22f
swopl <modaik5@gmail.com>
2021-12-21 23:03:43 +0100

    added encoding and decoding

--- ds/cmsketch/cmsketch.go
+++ ds/cmsketch/cmsketch.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"github.com/spaolacci/murmur3"
 	"hash"
+	"io"
 	"math"
+	"os"
 	"time"
 )
 
@@ -37,23 +39,23 @@ type CountMinSketch struct {
 	M, K uint
 	HashSeeds []uint32
 	hashes []hash.Hash32
-	Contents [][]uint
+	Contents [][]uint32
 }
 
 func New(epsilon, delta float64) *CountMinSketch {
 	m := CalculateM(epsilon)
 	k := CalculateK(delta)
 	hashes, seeds := CreateHashFunctions(k)
-	contents := make([][]uint, k)
+	contents := make([][]uint32, k)
 	for i := range contents {
-		contents[i] = make([]uint, m)
+		contents[i] = make([]uint32, m)
 	}
 	return &CountMinSketch{m, k, seeds, hashes, contents}
 }
 
-func (cms *CountMinSketch) Add(adding []byte) {
+func (cms *CountMinSketch) Add(element []byte) {
 	for i, v := range cms.hashes {
-		_, err := v.Write(adding)
+		_, err := v.Write(element)
 		if err != nil {
 			panic(err)
 		}
@@ -63,10 +65,11 @@ func (cms *CountMinSketch) Add(adding []byte) {
 	}
 }
 
-func (cms *CountMinSketch) EstimateFrequency(checking []byte) uint {
-	var min uint
+// EstimateFrequency of element in a stream of data
+func (cms *CountMinSketch) EstimateFrequency(element []byte) uint32 {
+	var min uint32
 	for i, v := range cms.hashes {
-		_, err := v.Write(checking)
+		_, err := v.Write(element)
 		if err != nil {
 			panic(err)
 		}
@@ -83,29 +86,87 @@ func (cms *CountMinSketch) EstimateFrequency(checking []byte) uint {
 }
 
 func (cms *CountMinSketch) EncodeToBytes() []byte {
-	// todo: test and implement rest
 	var outBin bytes.Buffer
 	encoder := gob.NewEncoder(&outBin)
 	err := encoder.Encode(cms)
-	encBytes := outBin.Bytes()
 	if err != nil {
-		return nil
+		panic(err)
 	}
+	encBytes := outBin.Bytes()
 	return encBytes
+}
+
+func (cms *CountMinSketch) EncodeToFile(filename string) {
+	outBin, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer func(outBin *os.File) {
+		err := outBin.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(outBin)
+	encoder := gob.NewEncoder(outBin)
+	err = encoder.Encode(cms)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Decode(reader io.Reader) *CountMinSketch {
+	decoder := gob.NewDecoder(reader)
+	cms := &CountMinSketch{}
+	err := decoder.Decode(cms)
+	if err != nil {
+		panic(err)
+	}
+	cms.hashes = make([]hash.Hash32, len(cms.HashSeeds))
+	for i, seed := range cms.HashSeeds {
+		cms.hashes[i] = murmur3.New32WithSeed(seed)
+	}
+	return cms
+}
+
+func DecodeFromBytes(data []byte) *CountMinSketch {
+	reader := bytes.NewReader(data)
+	return Decode(reader)
+}
+
+func DecodeFromFile(filename string) *CountMinSketch {
+	reader, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer func(reader *os.File) {
+		err := reader.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(reader)
+	return Decode(reader)
 }
 
 func main() {
 	cms := New(0.1, 0.1)
 	fmt.Println(cms.K)
 	fmt.Println(cms.M)
-	fmt.Println(cms.Contents)
 	cms.Add([]byte{1, 2})
-	fmt.Println(cms.Contents)
 	cms.Add([]byte{1, 2})
-	fmt.Println(cms.Contents)
 	cms.Add([]byte{3, 4})
 	fmt.Println(cms.Contents)
 	fmt.Println(cms.EstimateFrequency([]byte{1, 2}))
 	fmt.Println(cms.EstimateFrequency([]byte{3, 4}))
 	fmt.Println(cms.EstimateFrequency([]byte{2, 5}))
+	cmsBytes := cms.EncodeToBytes()
+	cms2 := DecodeFromBytes(cmsBytes)
+	fmt.Println(cms2.EstimateFrequency([]byte{1, 2}))
+	fmt.Println(cms2.EstimateFrequency([]byte{3, 4}))
+	fmt.Println(cms2.EstimateFrequency([]byte{2, 5}))
+	cms2.EncodeToFile("cms31451.bin")
+	cms3 := DecodeFromFile("cms31451.bin")
+	fmt.Println(cms3.EstimateFrequency([]byte{1, 2}))
+	fmt.Println(cms3.EstimateFrequency([]byte{3, 4}))
+	fmt.Println(cms3.EstimateFrequency([]byte{2, 5}))
+	fmt.Println(cms3.Contents)
 }
