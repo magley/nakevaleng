d1ce3036d9392d8c53f5d05811f367a27dc3fcca
magley <60552482+magley@users.noreply.github.com>
2022-02-17 11:51:49 +0100

    wrapped the cli in struct

---
+++ engine/wrappertest/clitest.go
@@ -0,0 +1,314 @@
+package wrappertest
+
+import (
+	"bufio"
+	"fmt"
+	"nakevaleng/ds/cmsketch"
+	hyperloglog "nakevaleng/ds/hll"
+	"nakevaleng/engine/wrappereng"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type cliState int
+
+const (
+	_GOOD     = 0
+	_BAD_CMD  = 1
+	_BAD_ARGC = 2
+	_BAD_ARGV = 3
+	_BAD_KEY  = 4
+	_ERROR    = 5
+)
+
+type CLITest struct {
+	eng     *wrappereng.WrapperEngine
+	args    []string
+	state   cliState
+	user    string
+	running bool
+}
+
+func NewCLI(user string, eng *wrappereng.WrapperEngine) *CLITest {
+	fmt.Println("Type 'help' for a list of commands.")
+	return &CLITest{
+		eng:     eng,
+		args:    make([]string, 0),
+		state:   _GOOD,
+		user:    user,
+		running: true,
+	}
+}
+
+func (cli CLITest) IsRunning() bool {
+	return cli.running
+}
+
+func (cli *CLITest) Next() {
+	// Print error and help
+
+	cli.handleError()
+
+	// Read input
+
+	cli.args = getNewArgs()
+	if len(cli.args) == 0 {
+		return
+	}
+
+	// Map input into function
+
+	funcmap := map[string]func() bool{
+		"help": cli.help,
+		"put":  cli.put,
+		"get":  cli.get,
+		"del":  cli.del,
+		"hllc": cli.hllc,
+		"hll":  cli.hll,
+		"cmsc": cli.cmsc,
+		"cms":  cli.cms,
+		"cmsq": cli.cmsq,
+		"quit": cli.quit,
+	}
+
+	// Call that function
+
+	cmd := cli.args[0]
+	f, has := funcmap[cmd]
+	if !has {
+		cli.state = _BAD_CMD
+		return
+	}
+
+	f()
+}
+
+func getNewArgs() []string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	line := scanner.Text()
+	return strings.Split(line, " ")
+}
+
+func (cli *CLITest) handleError() {
+	if cli.state == _GOOD {
+		return
+	} else if cli.state == _BAD_ARGC {
+		fmt.Println("Bad argument count!")
+	} else if cli.state == _BAD_ARGV {
+		fmt.Println("Bad argument value(s)!")
+	} else if cli.state == _BAD_CMD {
+		fmt.Println("Command not found!")
+	} else if cli.state == _BAD_KEY {
+		fmt.Println("Invalid key!")
+	} else if cli.state == _ERROR {
+		fmt.Println("Unexpected error!")
+	}
+
+	fmt.Println("Type 'help' for a list of commands.")
+
+	cli.state = _GOOD
+}
+
+func (cli CLITest) cmdHasArgc(n int) bool {
+	return len(cli.args)-1 == n
+}
+
+func (cli *CLITest) put() bool {
+	if !cli.cmdHasArgc(2) {
+		cli.state = _BAD_ARGC
+		return false
+	}
+
+	key := cli.args[1]
+	val := []byte(cli.args[2])
+	legalKey := cli.eng.Put(cli.user, key, val)
+
+	if !legalKey {
+		cli.state = _BAD_KEY
+		return false
+	}
+
+	return true
+}
+
+func (cli *CLITest) quit() bool {
+	if !cli.cmdHasArgc(0) {
+		cli.state = _BAD_ARGC
+		return false
+	}
+
+	cli.running = false
+	return true
+}
+
+func (cli *CLITest) get() bool {
+	if !cli.cmdHasArgc(1) {
+		cli.state = _BAD_ARGC
+		return false
+	}
+
+	key := cli.args[1]
+	rec, found := cli.eng.Get(cli.user, key)
+
+	if !found {
+		fmt.Println(key, "not found")
+		return false
+	} else {
+		fmt.Println(rec)
+	}
+
+	return true
+}
+
+func (cli *CLITest) del() bool {
+	if !cli.cmdHasArgc(1) {
+		cli.state = _BAD_ARGC
+		return false
+	}
+
+	key := cli.args[1]
+	found := cli.eng.Delete(cli.user, key)
+
+	if !found {
+		fmt.Println(key, "not found")
+		return false
+	}
+
+	return true
+}
+
+func (cli *CLITest) hllc() bool {
+	if !cli.cmdHasArgc(2) {
+		cli.state = _BAD_ARGC
+		return false
+	}
+
+	key := cli.args[1]
+	k, err := strconv.Atoi(cli.args[2])
+
+	if err != nil {
+		cli.state = _BAD_ARGV
+		return false
+	}
+	if k > hyperloglog.HLL_MAX_PRECISION || k < hyperloglog.HLL_MIN_PRECISION {
+		cli.state = _BAD_ARGV
+		return false
+	}
+
+	hll := hyperloglog.New(k)
+	cli.eng.PutHLL(cli.user, key, *hll)
+
+	return true
+}
+
+func (cli *CLITest) hll() bool {
+	// hll key     is for get
+	// hll key val is for set
+
+	if cli.cmdHasArgc(1) {
+		key := cli.args[1]
+		hll := cli.eng.GetHLL(cli.user, key)
+		if hll == nil {
+			fmt.Println(key, "not found.")
+			return false
+		}
+		fmt.Println(key, ": est =", hll.Estimate())
+		return true
+	} else if cli.cmdHasArgc(2) {
+		key := cli.args[1]
+		val := []byte(cli.args[2])
+		hll := cli.eng.GetHLL(cli.user, key)
+		if hll == nil {
+			fmt.Println(key, "not found.")
+			return false
+		}
+		hll.Add(val)
+		cli.eng.PutHLL(cli.user, key, *hll)
+		return true
+	} else {
+		cli.state = _BAD_ARGC
+		return false
+	}
+}
+
+func (cli *CLITest) cmsc() bool {
+	if !cli.cmdHasArgc(3) {
+		cli.state = _BAD_ARGC
+		return false
+	}
+
+	key := cli.args[1]
+	e, err := strconv.ParseFloat(cli.args[2], 64)
+	if err != nil {
+		cli.state = _BAD_ARGV
+		return false
+	}
+	d, err := strconv.ParseFloat(cli.args[3], 64)
+	if err != nil {
+		cli.state = _BAD_ARGV
+		return false
+	}
+	if e > 1.0 || e < 0.0 || d > 1.0 || d < 0.0 {
+		cli.state = _BAD_ARGV
+		return false
+	}
+
+	cms := cmsketch.New(e, d)
+	cli.eng.PutCMS(cli.user, key, *cms)
+
+	return true
+}
+
+func (cli *CLITest) cms() bool {
+	if !cli.cmdHasArgc(2) {
+		cli.state = _BAD_ARGC
+		return false
+	}
+
+	key := cli.args[1]
+	val := []byte(cli.args[2])
+	cms := cli.eng.GetCMS(cli.user, key)
+	if cms == nil {
+		fmt.Println(key, "not found.")
+		return false
+	}
+	cms.Insert(val)
+	cli.eng.PutCMS(cli.user, key, *cms)
+	return true
+}
+
+func (cli *CLITest) cmsq() bool {
+	if !cli.cmdHasArgc(2) {
+		cli.state = _BAD_ARGC
+		return false
+	}
+
+	key := cli.args[1]
+	val := []byte(cli.args[2])
+	cms := cli.eng.GetCMS(cli.user, key)
+	if cms == nil {
+		fmt.Println(key, "not found.")
+		return false
+	}
+	fmt.Println(key, ": est =", cms.Query(val))
+	return true
+}
+
+func (cli *CLITest) help() bool {
+	fmt.Println()
+	fmt.Println("help               -  view list of commands")
+	fmt.Println("put  [key] [val]   -  insert record")
+	fmt.Println("get  [key]         -  find record by key")
+	fmt.Println("del  [key]         -  delete record by key")
+	fmt.Println("hllc [key] [k]     -  create HLL object [key] with precision [k] (between 4 and 16)")
+	fmt.Println("hll  [key] [val]   -  put element [val] into HLL [key]")
+	fmt.Println("hll  [key]         -  get estimate for HLL [key]")
+	fmt.Println("cmsc [key] [e] [d] -  create CMS object [key] with epsilon [e] and delta [d] (both between 0.0 and 1.0)")
+	fmt.Println("cms  [key] [val]   -  put element [val] into CMS [key]")
+	fmt.Println("cmsq [key] [val]   -  get estimate for element [val] in CMS [key]")
+	fmt.Println("quit               -  exit program")
+
+	return true
+}
--- nakevaleng.go
+++ nakevaleng.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"nakevaleng/ds/cmsketch"
-	hyperloglog "nakevaleng/ds/hll"
 	"nakevaleng/engine/coreconf"
 	"nakevaleng/engine/wrappereng"
-	"strconv"
+	"nakevaleng/engine/wrappertest"
 )
 
 func main() {
 	eng := wrappereng.New(coreconf.LoadConfig("conf.yaml"))
-	//test_memtable_threshold(&eng)
 	test_cli(&eng)
 }
 
@@ -20,166 +16,9 @@ func test_cli(eng *wrappereng.WrapperEngine) {
 	// fmt.Println("[DBG]\t
 
 	user := "admin"
-	badCommand := false
+	cli := wrappertest.NewCLI(user, eng)
 
-	for true {
-		if badCommand {
-			fmt.Println()
-			fmt.Println("put  [key] [val]   -  insert record")
-			fmt.Println("get  [key]         -  find record by key")
-			fmt.Println("del  [key]         -  delete record by key")
-			fmt.Println("hllc [key] [k]     -  Create HLL object [key] with precision [k] (between 4 and 16)")
-			fmt.Println("hll  [key] [val]   -  Put element [val] into HLL [key]")
-			fmt.Println("hll  [key]         -  Get estimate for HLL [key]")
-			fmt.Println("cmsc [key] [e] [d] -  Create CMS object [key] with epsilon [e] and delta [d] (both 0-1)")
-			fmt.Println("cms  [key] [val]   -  Put element [val] into CMS [key]")
-			fmt.Println("cmsq [key] [val]   -  Get estimate for element [val] in CMS [key]")
-			fmt.Println("quit               -  Exit program")
-			badCommand = false
-		}
-		fmt.Print("\n>")
-
-		cmd, key, val, arg4 := "", "", "", ""
-		n, _ := fmt.Scanf("%s %s %s %s\n", &cmd, &key, &val, &arg4)
-
-		if cmd == "quit" {
-			break
-		} else if cmd == "put" {
-			if n != 3 {
-				badCommand = true
-				continue
-			}
-			eng.Put(user, key, []byte(val))
-			continue
-		} else if cmd == "get" {
-			if n != 2 {
-				badCommand = true
-				continue
-			}
-			rec, found := eng.Get(user, key)
-			if !found {
-				fmt.Println(key, "not found.")
-			} else {
-				fmt.Println(rec)
-			}
-			continue
-		} else if cmd == "del" {
-			if n != 2 {
-				badCommand = true
-				continue
-			}
-			found := eng.Delete(user, key)
-			if !found {
-				fmt.Println(key, "not found.")
-			} else {
-				fmt.Println(key, "removed.")
-			}
-		} else if cmd == "hllc" {
-			if n != 3 {
-				badCommand = true
-				continue
-			}
-			precision, err := strconv.Atoi(val)
-			if err != nil {
-				badCommand = true
-				continue
-			}
-			if precision > hyperloglog.HLL_MAX_PRECISION || precision < hyperloglog.HLL_MIN_PRECISION {
-				badCommand = true
-				continue
-			}
-			hll := hyperloglog.New(precision)
-			eng.PutHLL(user, key, *hll)
-		} else if cmd == "hll" {
-			if n == 3 {
-				hll := eng.GetHLL(user, key)
-				if hll == nil {
-					fmt.Println(key, "not found.")
-					continue
-				}
-				hll.Add([]byte(val))
-				eng.PutHLL(user, key, *hll)
-			} else if n == 2 {
-				hll := eng.GetHLL(user, key)
-				if hll == nil {
-					fmt.Println(key, "not found.")
-					continue
-				}
-				fmt.Println(key, "estimate:", hll.Estimate())
-			} else {
-				badCommand = true
-				continue
-			}
-			continue
-		} else if cmd == "cmsc" {
-			if n != 4 {
-				badCommand = true
-				continue
-			}
-			epsilon, err := strconv.ParseFloat(val, 32)
-			if err != nil {
-				badCommand = true
-				continue
-			}
-			delta, err := strconv.ParseFloat(arg4, 32)
-			if err != nil {
-				badCommand = true
-				continue
-			}
-			if epsilon < 0 || epsilon > 1 || delta < 0 || delta > 1 {
-				badCommand = true
-				continue
-			}
-			cms := cmsketch.New(epsilon, delta)
-			eng.PutCMS(user, key, *cms)
-
-		} else if cmd == "cms" {
-			if n != 3 {
-				badCommand = true
-				continue
-			}
-
-			cms := eng.GetCMS(user, key)
-			if cms == nil {
-				fmt.Println(key, "not found.")
-				continue
-			}
-			cms.Insert([]byte(val))
-			eng.PutCMS(user, key, *cms)
-			continue
-		} else if cmd == "cmsq" {
-			if n != 3 {
-				badCommand = true
-				continue
-			}
-
-			cms := eng.GetCMS(user, key)
-			if cms == nil {
-				fmt.Println(key, "not found.")
-				continue
-			}
-			fmt.Println(key, "estimate:", cms.Query([]byte(val)))
-			continue
-		} else {
-			badCommand = true
-			continue
-		}
+	for cli.IsRunning() {
+		cli.Next()
 	}
-}
-
-func test_memtable_threshold(eng *wrappereng.WrapperEngine) {
-	// 3 elements (way less than memtable capacity)
-	// But in total they take up 3000 B
-	// If memtable threshold is 2000 B, then a flush will happen.
-	// Token buckets are also written though.
-	// CMD output is written in memtable.go @ line 39
-
-	val1 := make([]byte, 1000)
-	val2 := make([]byte, 1000)
-	val3 := make([]byte, 1000)
-	user := "admin"
-
-	eng.Put(user, "key1", val1)
-	eng.Put(user, "key2", val2)
-	eng.Put(user, "key3", val3)
 }
